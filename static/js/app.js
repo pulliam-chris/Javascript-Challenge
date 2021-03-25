732846146526b547f6099c3d53105927bb3b0f45
Chris Pulliam <chrispulliam.jobs@gmail.com>
2021-03-25 13:48:32 -0700

    started dynamic table build.  appears to be working

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,28 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+console.log(tableData);
+
+// Use D3 to select the table body
+let tbody = d3.select("tbody");
+
+// Use D3 to select the table
+let table = d3.select("ufo-table");
+
+// Use D3 to set the table class to `table table-striped`
+table.attr("class", "table table-striped");
+
+// Iterate through each ufo encounter in list
+tableData.forEach((encounter) => {
+  
+  // Append one table row per ufo encounter
+  let row = tbody.append("tr");
+
+  // Iterate through encounter array to build data
+  Object.values(encounter).forEach(value => {
+    let cell = row.append("td");
+         cell.text(value);
+       });
+
+});
+
