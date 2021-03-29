7d142f93f7fda601f3b7671550b5e8247d32c21e
Chris Pulliam <chrispulliam.jobs@gmail.com>
2021-03-29 14:36:58 -0700

    cleaned code, added extra l2 filter fields and logic to code

--- static/js/app.js
+++ static/js/app.js
@@ -3,8 +3,6 @@ var tableData = data,
     button = d3.select("#filter-btn"),
     form = d3.select("#filter list-group-item");
 
-// console.log(tableData);
-
 // Use D3 to select the table body
 let tbody = d3.select("tbody");
 
@@ -34,31 +32,54 @@ const filterTable = () => {
 
   // Select the input element and get the raw HTML node
   // Get the value property of the input element
-  let inputElement = d3.select("#datetime"),
+  let inputElement = d3.select("#datetime");
   inputValue = inputElement.property("value");
 
-  //console.log(inputValue);
-  
-  // Purge the existing table for filtered data
-  //tableData.forEach((encounter) => {
-  //  let removeRow = tbody("tr");
-  //  removeRow.remove();
-  //})
-  
-  let filteredData = tableData.filter(encounter => encounter.datetime === inputValue);
+  let inputCity = d3.select("#city");
+  cityValue = inputCity.property("value");
+  //console.log(cityValue);
 
- //console.log(filteredData);
+  let inputState = d3.select("#state");
+  stateValue = inputState.property("value");
 
-  // Use D3 to select the table body
-  //tbody = d3.selectAll("tr").remove();
-  //let tbody = d3.select("tbody");
+  let inputCountry = d3.select("#country");
+  countryValue = inputCountry.property("value");
+
+  let inputShape = d3.select("#shape");
+  shapeValue = inputShape.property("value");
+
+  // Filter data based on input values  
+  let filteredData = tableData;
+  
+  // Only attempt to filter if a value was input
+  if (inputValue !== "") {
+    filteredData = filteredData.filter(encounter => encounter.datetime === inputValue);
+  }
+
+  if (cityValue !== "") {
+    filteredData = filteredData.filter(encounter => encounter.city === cityValue);
+  }
+
+  if (stateValue !== "") {
+    filteredData = filteredData.filter(encounter => encounter.state === stateValue);
+  }
+
+  if (countryValue !== "") {
+    filteredData = filteredData.filter(encounter => encounter.country === countryValue);
+  }
+
+  if (shapeValue !== "") {
+    filteredData = filteredData.filter(encounter => encounter.shape === shapeValue);
+  }
   
   // Use D3 to select the table
-  let table = d3.select("ufo-table");
+  // let table = 
+  d3.select("ufo-table");
   
   // Purge the existing rows to be replaced by filtered data
-  currentRows = d3.select("tbody").selectAll("tr").remove();
-  //currentRows = tbody.remove("tr");
+  // let currentRows = 
+  d3.select("tbody").selectAll("tr").remove();
+  //list.html("");
 
   // Use D3 to set the table class to `table table-striped`
   //table.attr("class", "table table-striped");
